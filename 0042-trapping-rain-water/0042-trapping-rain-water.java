3dcd64812890935695575b65a1bcbe377837979c
Archita Verma <archita6607@gmail.com>
2026-08-02 21:49:32 +0530

    Time: 1 ms (59.26%), Space: 48.8 MB (9.46%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int trap(int[] height) {
+        int n=height.length;
+        int w=0;
+        int leftmax[]=new int [n];
+        leftmax[0]=height[0];
+        for(int i=1;i<n;i++){
+            leftmax[i]=Math.max(leftmax[i-1],height[i]);
+        }
+        int rightmax[]=new int [n];
+        rightmax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightmax[i]=Math.max(rightmax[i+1],height[i]);
+        }
+        for(int i=0;i<n;i++){
+            w+=Math.min(leftmax[i],rightmax[i])-height[i];
+        }
+        return w;
+    }
+}
