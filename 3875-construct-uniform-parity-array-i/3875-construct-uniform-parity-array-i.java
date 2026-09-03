10dca7546bc562b9fadaf7f5b04e493f756bd381
Archita Verma <archita6607@gmail.com>
2026-09-03 05:45:58 +0530

    Time: 1 ms (32.15%), Space: 44.8 MB (94.08%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,5 +1,35 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        return true;
+        int n=nums1.length;
+        int []nums2=new int[n];
+        return solve(nums1,0,nums2);
     }
-}
+    boolean solve(int []nums1,int index,int []nums2 ){
+        if(index==nums1.length){
+            return sameParity(nums2);
+        }
+        nums2[index]=nums1[index];
+        if(solve(nums1,index+1,nums2)){
+            return true;
+        }
+        for(int j=0;j<nums1.length;j++){
+            if(j==index){
+                continue;
+            }
+            nums2[index]=nums1[index]-nums1[j];
+            if(solve(nums1,index+1,nums2)){
+                return true;
+            }
+        }
+            return false;
+        }
+        boolean sameParity(int []nums){
+            int parity=nums[0]%2;
+            for(int i=0;i<nums.length;i++){
+                if(nums[0]%2!=parity){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
